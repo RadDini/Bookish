e41ea50de59be4310e35c9d5977c35f4324d0d88
RadDini <radu.dinicescu@softwire.com>
2024-07-08 13:37:40 +0300

    bookish - user db and register

--- bookish/controllers/bookish.py
+++ bookish/controllers/bookish.py
@@ -1,6 +1,9 @@
 from flask import request
 from bookish.models.example import Example
-from bookish.models import db
+from bookish.models.user import Users
+from bookish.models import db, user
+
+import bcrypt
 
 
 def bookish_routes(app):
@@ -29,3 +32,43 @@ def bookish_routes(app):
                     'data2': example.data2
                 } for example in examples]
             return {"examples": results}
+
+    @app.route('/register', methods=['POST'])
+    def handle_register():
+        if request.method == 'POST':
+            if request.is_json:
+                data = request.get_json()
+                salt = bcrypt.gensalt()
+                bytes = data['password'].encode('utf-8')
+
+                hashed = bcrypt.hashpw(bytes, salt)
+                password = hashed.decode("utf-8", "ignore")
+
+                new_user = Users(name=data['name'], password=password, email=data['email'])
+                db.session.add(new_user)
+                db.session.commit()
+                return {"message": "New user has been created successfully."}
+            else:
+                return {"error": "The request payload is not in JSON format"}
+
+        else:
+            return {"error": "Method not allowed"}
+
+    @app.route('/get_users', methods=['GET'])
+    def handle_get_users():
+        if request.method == 'GET':
+            users = Users.query.all()
+            results = [{
+                    'id': user.id,
+                    'Name': user.name,
+                    'Password': user.password,
+                    'Email': user.email
+                } for user in users]
+
+            return {"users": results}
+
+
+        else:
+            return {"error": "Method not allowed"}
+
+
---
+++ bookish/models/user.py
@@ -0,0 +1,28 @@
+from bookish.app import db
+
+
+class Users(db.Model):
+    # This sets the name of the table in the database
+    __tablename__ = 'Users'
+
+    # Here we outline what columns we want in our database
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+
+    def __init__(self, name, password, email):
+        self.name = name
+        self.password = password
+        self.email = email
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'Name': self.name,
+            'Password': self.password,
+            'Email': self.email
+        }
