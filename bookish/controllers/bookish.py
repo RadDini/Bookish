32404f1d994229417dbf260543e5e8474ca9cc64
RadDini <radu.dinicescu@softwire.com>
2024-07-08 12:28:26 +0300

    setup done

--- bookish/controllers/bookish.py
+++ bookish/controllers/bookish.py
@@ -6,7 +6,7 @@ from bookish.models import db
 def bookish_routes(app):
     @app.route('/healthcheck')
     def health_check():
-        return {"status": "OK"}
+        return {"status": "OK vreau acasa"}
 
     @app.route('/example', methods=['POST', 'GET'])
     def handle_example():
